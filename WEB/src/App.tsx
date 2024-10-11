12a0a4aa17a11f12929b53b3bd2a9249c3220663
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-10-11 15:01:13 -0300

    novo front

---
+++ WEB/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+export function App() {
+  return (
+    <div>
+      <h1>Ola mundo</h1>
+      <Button>Ola</Button>
+    </div>
+  )
+}
