d8d0e08628794d6c81bfd98b383e9ce09ff3aa8e
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-10-22 11:26:25 -0300

    quero ajudar

--- WEB/src/pages/app/quero-adotar.tsx
+++ WEB/src/pages/app/quero-adotar.tsx
@@ -86,15 +86,15 @@ const listPets = [
 export function QueroAdotar() {
     const [pet, setPet] = useState([])
     return (
-        <div className="min-w-full mt-10 mb-5">
+        <div className="min-w-full mt-10 mb-5 flex flex-col items-center">
             <Helmet title="QueroAdotar" />
-            <h2 className="font-bold text-[30px] ml-36">Conheça nossos belos companheirinhos</h2>
-                <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-5 mt-5 justify-items-center">
+            <h2 className="font-bold text-[30px]">Conheça nossos belos companheirinhos</h2>
+                <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-5 m-4 mt-5 justify-items-center">
                     {
                         listPets.map((listPet) => (
-                            <Link to={'/'} className="w-[23rem] h-[15rem] bg-blue-300 flex flex-col items-center justify-center rounded-[20px]">
-                                <img className="w-[19rem] rounded-[20px]" src={listPet.fotos} alt={listPet.name} />
-                                <p className="font-semibold text-[20px]">{listPet.name}</p>
+                            <Link to={'/'} className="w-[20rem] h-[15rem] bg-primary flex flex-col items-center justify-center rounded-[10px]">
+                                <img className="w-[19rem] rounded-[5px]" src={listPet.fotos} alt={listPet.name} />
+                                <p className="font-semibold text-[20px] text-muted">{listPet.name}</p>
                             </Link>
                         ))
                     }
--- WEB/src/pages/app/quero-ajudar.tsx
+++ WEB/src/pages/app/quero-ajudar.tsx
@@ -1,10 +1,103 @@
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+
+const listPets = [
+    {
+        id: "f1v3gjhi8iake0800eq7vt6a",
+        name: "toto 2",
+        idade: "1 ano",
+        peso: "20 kg",
+        porte: "grande",
+        tipo: "Cachorro",
+        descricao: "Um lindo e grande dog preto e branco e cheio de amor pra dar.",
+        requisitos: "Ter lucar espaçoso",
+        fotos: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+        userId: "hr84i3xjfohzgi6s9zhijo4m",
+        adotado: false
+    },
+    {
+        id: "f1v3gjhi8iake0800eq7vt6a",
+        name: "toto 2",
+        idade: "1 ano",
+        peso: "20 kg",
+        porte: "grande",
+        tipo: "Cachorro",
+        descricao: "Um lindo e grande dog preto e branco e cheio de amor pra dar.",
+        requisitos: "Ter lucar espaçoso",
+        fotos: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+        userId: "hr84i3xjfohzgi6s9zhijo4m",
+        adotado: false
+    },
+    {
+        id: "f1v3gjhi8iake0800eq7vt6a",
+        name: "toto 2",
+        idade: "1 ano",
+        peso: "20 kg",
+        porte: "grande",
+        tipo: "Cachorro",
+        descricao: "Um lindo e grande dog preto e branco e cheio de amor pra dar.",
+        requisitos: "Ter lucar espaçoso",
+        fotos: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+        userId: "hr84i3xjfohzgi6s9zhijo4m",
+        adotado: false
+    },
+    {
+        id: "f1v3gjhi8iake0800eq7vt6a",
+        name: "toto 2",
+        idade: "1 ano",
+        peso: "20 kg",
+        porte: "grande",
+        tipo: "Cachorro",
+        descricao: "Um lindo e grande dog preto e branco e cheio de amor pra dar.",
+        requisitos: "Ter lucar espaçoso",
+        fotos: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+        userId: "hr84i3xjfohzgi6s9zhijo4m",
+        adotado: false
+    },
+    {
+        id: "f1v3gjhi8iake0800eq7vt6a",
+        name: "toto 2",
+        idade: "1 ano",
+        peso: "20 kg",
+        porte: "grande",
+        tipo: "Cachorro",
+        descricao: "Um lindo e grande dog preto e branco e cheio de amor pra dar.",
+        requisitos: "Ter lucar espaçoso",
+        fotos: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+        userId: "hr84i3xjfohzgi6s9zhijo4m",
+        adotado: false
+    },
+    {
+        id: "f1v3gjhi8iake0800eq7vt6a",
+        name: "toto 2",
+        idade: "1 ano",
+        peso: "20 kg",
+        porte: "grande",
+        tipo: "Cachorro",
+        descricao: "Um lindo e grande dog preto e branco e cheio de amor pra dar.",
+        requisitos: "Ter lucar espaçoso",
+        fotos: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+        userId: "hr84i3xjfohzgi6s9zhijo4m",
+        adotado: false
+    }
+]
 
 export function QueroAjudar() {
     return (
-        <>
-            <Helmet title="QueroAjudar" />
-            <h1>QueroAjudar</h1>
-        </>
+        <div className="min-w-full mt-10 mb-5 flex flex-col items-center">
+            <Helmet title="Quero Ajudar" />
+            <h2 className="font-bold text-[30px]">Ongs parceiras</h2>
+                <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-5 m-4 mt-5 justify-items-center">
+                    {
+                        listPets.map((listPet) => (
+                            <Link to={'/'} className="w-[20rem] h-[15rem] bg-muted-foreground flex flex-col items-center justify-center rounded-[10px]">
+                                <img className="w-[19rem] rounded-[5px]" src={listPet.fotos} alt={listPet.name} />
+                                <p className="font-semibold text-[20px] text-muted">{listPet.name}</p>
+                            </Link>
+                        ))
+                    }
+                </div>
+        </div>
     )
 }
